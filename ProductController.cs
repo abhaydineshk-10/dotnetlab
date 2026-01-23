e11b63789156d80c11828782ac7253185086a559
Abhayy <eyeronic2772@gmail.com>
2026-01-23 13:04:32 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { Id = 1, Name= "Laptop", Price= 60000, Quantity=500 },
+                new Product { Id = 2, Name= "Mobile", Price= 30000, Quantity=300 },
+                new Product { Id = 3, Name= "Headphones", Price= 3000, Quantity= 700}
+            };
+
+            return View(products);
+        }
+    }
+}
