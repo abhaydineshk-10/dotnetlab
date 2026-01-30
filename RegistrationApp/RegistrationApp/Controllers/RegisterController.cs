5500822d4e972c485736975162764873fcece7e1
Abhayy <eyeronic2772@gmail.com>
2026-01-30 12:50:04 +0530

    Add files via upload

---
+++ RegistrationApp/RegistrationApp/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegistrationApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Models.Register User)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content(
+                $"Registration Sucessful\nName: {User.Name}\nEmail: {User.Email}\nPassword: {User.Password}\nAge: {User.Age}");
+            }
+            return View(User);
+
+        }
+    }
+}
---
+++ RegistrationApp/RegistrationApp/Models/Register.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistrationApp.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(@"^[A-Za-z]+$",
+            ErrorMessage = "Name must contain only alphabets")]
+        public string Name {  get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(
+            @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{6,}$",
+            ErrorMessage ="Password must contain at least one uppercase, one lowercase letter and one number and one special character ")]
+        [MinLength(6,ErrorMessage ="Password must be atleast 6 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage ="Age is required")]
+        [Range(18, 60, ErrorMessage ="Age must be between 18 and 60")]
+
+        public int Age { get; set; }
+    }
+}
