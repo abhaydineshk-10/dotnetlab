92c9fe9632f2b5a810efe91de64621b325d50e50
Abhayy <eyeronic2772@gmail.com>
2026-03-06 11:52:37 +0530

    Add files via upload

---
+++ Cookie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cookie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
