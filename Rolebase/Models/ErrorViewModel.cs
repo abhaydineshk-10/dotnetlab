341378e6fc9b41c43b4cca74fb88f4ddc6097640
Abhayy <eyeronic2772@gmail.com>
2026-03-06 12:26:05 +0530

    Add files via upload

---
+++ Rolebase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rolebase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
