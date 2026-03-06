80b903ac08f1d1552723cfc2178363adae559aa7
Abhayy <eyeronic2772@gmail.com>
2026-03-06 11:48:00 +0530

    Add files via upload

---
+++ AppSettings/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppSettings.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
